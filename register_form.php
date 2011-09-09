c3e6cf120d988c3f05e45c9fa5b44ca57542ad08
Justin Derby <justderb@iastate.edu>
2011-09-08 21:23:10 -0500

    Login PHP test

---
+++ register_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+  <head> 
+    <meta charset="utf-8"> 
+    <title>Pencl - Login</title> 
+  </head> 
+<body>
+<form name="register" action="register.php" method="post">
+	Username: <input type="text" name="username" maxlength="30" ></input>
+	<br>Password: <input type="password" name="pass1" ></input>
+	<br>Password Again: <input type="password" name="pass2"></input>
+	<br>
+	<input type="submit" value="Register" ></input>
+</form>
+</body>
+</html>
