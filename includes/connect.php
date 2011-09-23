bbe3cff87407565670c632493af633f37060db72
Justin Derby <justderb@iastate.edu>
2011-09-22 23:17:40 -0500

    Minor bug fix

--- includes/connect.php
+++ includes/connect.php
@@ -15,7 +15,7 @@ if (mysqli_connect_errno()) {
 
 $mysqli->set_charset("utf8");
 
-mysql_connect($db_host, $db_user, $db_pass) or die('Error connecting to mysql, try again later.');
+$link = mysql_connect($db_host, $db_user, $db_pass) or die('Error connecting to mysql, try again later.');
 mysql_select_db($db_name);
 
 ?>
