40afd48c9b08170d57fc4c86df7162a79482be7f
Taylor <tbouvin@iastate.edu>
2011-10-27 23:05:38 -0500

    Modify load with ajax

--- test.php
+++ test.php
@@ -16,10 +16,31 @@ function ajaxLoad() {
 
     // Do you ajax call here, window.setTimeout fakes ajax call
     ed.setProgressState(1); // Show progress
-    window.setTimeout(function() {
+   /* window.setTimeout(function() {
         ed.setProgressState(0); // Hide progress
         ed.setContent('HTML content that got passed from server.');
-    }, 3000);
+    }, 3000);*/
+    $.ajax({
+		url: "util/notepadPost.php",
+		statusCode: {
+			404: function() {
+				alert('page not found');
+			},
+			409: function(data) {
+				alert('409 ' + data);
+			},
+			200: function(data) {
+				alert('200 ' + data);
+			}
+		},
+		data: {
+			//key1: "value1",
+			error : 'true'
+		},
+		complete: function(jqXHR, textStatus) {
+			ed.setProgressState(0);
+		}
+    });
 }
 
 function ajaxSave() {
