abd9d459d51ceb8ab7f490eb9717b62d7df9852f
Justin Derby <justderb@iastate.edu>
2011-11-18 21:29:07 -0600

    Login page now looks like something!

--- login.php
+++ login.php
@@ -10,6 +10,7 @@ include 'includes/registerFunctions.php';
 <title>Pencl - Coming Soon</title>
 <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/login.css" media="screen">
 <?php
 include 'includes/topbar_header.php';
 include 'includes/tracker.php';
@@ -29,13 +30,38 @@ if($_SESSION['msg']['login-err'])
 }
 ?>
 
-
-<div id="pagewide">		
-<form action="" method="post">
-	Username: <input name="username" type="text"><br>
-	Password: <input name="password" type="password"><br>
-	<input name="submit" type="submit" value="Login"></form>
-
+<br>
+<br>
+<div class="login">		
+	<div class="book">
+		<div class="page">
+			<h1>Register (Doesn't work yet)</h1>
+			<br>
+			<form action="" method="post">
+				<p>Username:</p>
+				<input name="username" type="text"><br>
+				<p>Password:</p>
+				<input name="password" type="password"><br>
+				<p>Confirm Password:</p>
+				<input name="passwordagain" type="password"><br>
+				<p>Beta Key:</p>
+				<input name="betaKey" type="text"><br>
+				<input class="submit" name="submit" type="submit" value="Register">
+			</form>
+		</div>
+		<div class="middle"><br></div>
+		<div class="page">
+			<h1>Login</h1>
+			<br>
+			<form action="" method="post">
+				Username: <input name="username" type="text"><br>
+				Password: <input name="password" type="password"><br>
+				<input class="submit" name="submit" type="submit" value="Login">
+			</form>
+		</div>
+		<div style="clear:both"></div>
+	</div>
+	
 </div>
 </body>
 </html>
