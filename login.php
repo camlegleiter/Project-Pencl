47d9ee428b38c6cbde53b0900cb424287dfb44be
Justin Derby <justderb@iastate.edu>
2011-11-19 16:08:05 -0600

    Register now works and focuses on correct form

--- login.php
+++ login.php
@@ -11,23 +11,29 @@ include 'includes/registerFunctions.php';
 <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
 <link rel="stylesheet" type="text/css" href="css/login.css" media="screen">
+<!-- Load jQuery -->
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript">
+	google.load("jquery", "1");
+</script>
+
 <?php
 include 'includes/topbar_header.php';
 include 'includes/tracker.php';
 ?>
 </head>
 
-<body>
+<body onload="
+<?php 
+	if ($_SESSION['msg']['reg-err'])
+		echo "$('#usernameRegister').focus()";
+	else
+		echo "$('#usernameLogin').focus()";
+?>
+">
 <?php
 //Must be first thing in the <body> tag to function correctly
 include 'includes/topbar.php';
-
-
-if($_SESSION['msg']['login-err'])
-{
-	echo '<div class="error"><p>'.$_SESSION['msg']['login-err'].'</p></div>';
-	unset($_SESSION['msg']['login-err']);
-}
 ?>
 
 <br>
@@ -35,11 +41,11 @@ if($_SESSION['msg']['login-err'])
 <div class="login">		
 	<div class="book">
 		<div class="page">
-			<h1>Register (Doesn't work yet)</h1>
+			<h1>Register</h1>
 			<br>
 			<form action="" method="post">
 				<p>Username:</p>
-				<input name="username" type="text"><br>
+				<input id="usernameRegister" name="username" type="text"><br>
 				<p>Password:</p>
 				<input name="password" type="password"><br>
 				<p>Confirm Password:</p>
@@ -48,16 +54,38 @@ if($_SESSION['msg']['login-err'])
 				<input name="betaKey" type="text"><br>
 				<input class="submit" name="submit" type="submit" value="Register">
 			</form>
+			<br>
+			<?php
+				if ($_SESSION['msg']['reg-success'])
+				{
+					echo '<div class="success"><p>'.$_SESSION['msg']['reg-success'].'</p></div>';
+					unset($_SESSION['msg']['reg-success']);
+				}
+				if($_SESSION['msg']['reg-err'])
+				{
+					echo '<div class="error"><p>'.$_SESSION['msg']['reg-err'].'</p></div>';
+					unset($_SESSION['msg']['reg-err']);
+				}
+
+			?>
 		</div>
 		<div class="middle"><br></div>
 		<div class="page">
 			<h1>Login</h1>
 			<br>
 			<form action="" method="post">
-				Username: <input name="username" type="text"><br>
+				Username: <input id="usernameLogin" name="username" type="text"><br>
 				Password: <input name="password" type="password"><br>
 				<input class="submit" name="submit" type="submit" value="Login">
 			</form>
+			<br>
+			<?php
+				if($_SESSION['msg']['login-err'])
+				{
+					echo '<div class="error"><p>'.$_SESSION['msg']['login-err'].'</p></div>';
+					unset($_SESSION['msg']['login-err']);
+				}
+			?>
 		</div>
 		<div style="clear:both"></div>
 	</div>
