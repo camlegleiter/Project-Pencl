64ad6cd9b9f9ad329815488264539fa89466a265
Justin Derby <justderb@iastate.edu>
2011-11-21 13:32:08 -0600

    deleted

--- table.sql
+++
@@ -1,9 +0,0 @@
---
--- Table structure for table `coming_soon_emails`
---
-
-CREATE TABLE `coming_soon_emails` (
-  `email` varchar(64) collate utf8_unicode_ci NOT NULL,
-  `ts` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
