73a88fb9a391217aa48d2460961c0cf93172fa9f
Taylor <tbouvin@iastate.edu>
2011-11-21 16:55:58 -0600

    updated share.php

--- share.php
+++ share.php
@@ -4,6 +4,7 @@ include 'includes/headerbarFunctions.php';
 //Include this inside the <head> tag to require user to be logged in to view the page.
 include 'includes/membersOnly.php';
 
+define('INCLUDE_CHECK', true);
 include 'includes/functions.php';
 
 function printAllClasses($userid)
