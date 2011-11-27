e036b25264980438ede65621474352884b44fcf2
Cameron <cameron@Camerons-MacBook.local>
2011-11-26 18:37:27 -0600

    Fixed duplicate entry display when chosing classes to share

--- share.php
+++ share.php
@@ -10,16 +10,16 @@ function printAllClasses($userid)
 {
 	$userid = mysql_real_escape_string($userid);
 	$padRow = mysql_query("SELECT classid FROM classmates WHERE userid='$userid'");
-	$padRow2 = mysql_query("SELECT owner FROM classes WHERE owner='$userid'");
+	$padRow2 = mysql_query("SELECT id FROM classes WHERE owner='$userid'");
 	$classmatesHTML = "";
 	
 	while ($row = mysql_fetch_assoc($padRow2))
 	{
-		$classmatesHTML .= getClassRow($userid, $row['owner']);
+		$classmatesHTML .= 'x' . getClassRow($userid, $row['id']);
 	}
 	while ($row = mysql_fetch_assoc($padRow))
 	{
-		$classmatesHTML .= getClassRow($userid, $row['classid']);
+		$classmatesHTML .= 'y' . getClassRow($userid, $row['classid']);
 	}
 	
 	mysql_free_result($padRow);
@@ -40,7 +40,7 @@ function getClassRow($userid, $classid)
 		$rowHTML = '
 			<tr>
 				<td>
-				<input type="checkbox" name="share" value="Share" /> Share
+				<input type="checkbox" name="share"/>
 				</td>
 				<td align="left">
 					'.$row['name'].'
@@ -60,14 +60,14 @@ function getClassRow($userid, $classid)
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Notepad Selection - Pencl</title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Notepad Selection - Pencl</title>
 
-<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
-<link rel="stylesheet" type="text/css" href="css/dialog/jqModal.css">
+		<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen">
+		<link rel="stylesheet" type="text/css" href="css/dialog/jqModal.css">
 
-<script type="text/javascript" src="js/dialog/jqModal.js"></script>
+		<script type="text/javascript" src="js/dialog/jqModal.js"></script>
 <?php
 //Must be in header!
 include 'includes/topbar_header.php';
@@ -77,44 +77,45 @@ include 'includes/topbar_header.php';
 //Put this at the end of the <head> tag to track
 include 'includes/tracker.php';
 ?>
-</head>
+	</head>
 
-<body>
+	<body>
 <?php
 //Must be first thing in the <body> tag to function correctly
 include 'includes/topbar.php';
 ?>
-<div id="pagewide">
-	<h1>Which classes would you like to share with?</h1>
+		<div id="pagewide">
+			<h1>Which classes would you like to share with?</h1>
 	
-	<div class="notebook">
-		<table>
-			<thead>
-				<tr class="head">
-					<td>
-						<!-- Preview -->
-                        <strong>Share</strong>
-                        
-					</td>
-					<td>
-						<strong>Classes</strong>
-					</td>
-					<td>
-						<strong>Description</strong>
-					</td>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					//Grab our notepads
-					echo printAllClasses($_SESSION['id']);
-				?>
-                
-			</tbody>
-		</table>
-	</div>
-	<br>
-	<p>Tip: Choose other options in the drop down menu at the top-right!</p>
-</div>
-</body>
+			<div class="notebook">
+				<form method="POST" action="">
+					<table>
+						<thead>
+							<tr class="head">
+								<td>
+									<!-- Preview -->
+								</td>
+								<td>
+									<strong>Classes</strong>
+								</td>
+								<td>
+									<strong>Description</strong>
+								</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								//Grab our notepads
+								echo printAllClasses($_SESSION['id']);
+							?>
+						    
+						</tbody>
+					</table>
+					<input type="submit" value="Submit">
+				</form>
+			</div>
+			<br>
+			<p>Tip: Choose other options in the drop down menu at the top-right!</p>
+		</div>
+	</body>
 </html>
